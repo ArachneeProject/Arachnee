d36740a36d005e6205e8c63329cb0725210cfe8d
SuperValou <email>
2017-10-30 23:45:07 +0100

    Set up basic keyboard/mouse controller and add simple LookAt script

---
+++ Arachnee/Assets/Classes/SceneScripts/LookAtCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Classes.SceneScripts
+{
+    public class LookAtCamera : MonoBehaviour
+    {
+        public bool lookAway = true;
+
+        void Update()
+        {
+            var positionToLookAt = lookAway
+                ? 2 * this.transform.position - Camera.main.transform.position
+                : Camera.main.transform.position;
+
+            this.transform.LookAt(positionToLookAt);
+        }
+    }
+}
---
+++ Arachnee/Assets/Classes/SceneScripts/MouseAndKeyboardController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Assets.Classes.SceneScripts
+{
+    public class MouseAndKeyboardController : MonoBehaviour 
+    {
+        // keyboard
+        public bool canMove = true;
+        public float keyboardSensitivity = 0.8F;
+
+        // mouse
+        public bool canLookAround = true;
+
+        public float sensitivityX = 2F;
+        public float sensitivityY = -2F;
+        
+        public float minimumY = -60F;
+        public float maximumY = 60F;
+
+        float previousYAngle = 0F;
+        
+        void Update()
+        {
+            // keyboard
+            if (this.canMove)
+            {
+                this.transform.position += this.transform.forward * Input.GetAxis("Vertical") * keyboardSensitivity;
+                this.transform.position += this.transform.right * Input.GetAxis("Horizontal") * keyboardSensitivity;
+            }
+
+            // mouse
+            if (canLookAround && Input.GetMouseButton(0))
+            {
+                float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+                previousYAngle += Input.GetAxis("Mouse Y") * sensitivityY;
+                previousYAngle = Mathf.Clamp(previousYAngle, minimumY, maximumY);
+                transform.localEulerAngles = new Vector3(previousYAngle, rotationX, 0);
+            }
+        }
+    }
+}
